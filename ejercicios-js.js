8884f03339599ebb5edc82c5f65e4c8fbd26863e
kerwuin <kerwuin1992@outlook.com>
2023-06-14 00:06:45 -0400

    Reverse a number

---
+++ ejercicios-js.js
@@ -0,0 +1,13 @@
+// Reverse Number
+
+function reverseNumber(n) { 
+  
+  n = n + "";
+  
+    return (
+    n.split("").reverse().join("")
+  );
+}
+
+console.log(reverseNumber(32243))
+console.log(reverseNumber(12345))
